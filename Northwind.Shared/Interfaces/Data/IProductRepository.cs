e083806b92bf7f481fe907546975d085ad42953a
Steffen Jørgensen <dk.steffen@gmail.com>
2021-11-26 15:54:44 +0100

    Interfaces for product

---
+++ Northwind.Shared/Interfaces/Data/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Northwind.Shared.DomainModels;
+
+namespace Northwind.Shared.Interfaces.Data
+{
+    public interface IProductRepository
+    {
+        Product GetProduct(int id);
+        Task<IEnumerable<Product>> GetAllProducts();
+    }
+}
---
+++ Northwind.Shared/Interfaces/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Northwind.Shared.DomainModels;
+
+namespace Northwind.Shared.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Product GetProduct(int id);
+        Task<IEnumerable<Product>> GetAllProducts();
+    }
+}
